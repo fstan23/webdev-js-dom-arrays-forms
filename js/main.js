3f351afa65cbb2d3e22a671d13dd111750127278
Felix Standen <c3032522@hallam.shu.ac.uk>
2023-11-17 10:25:37 +0000

    More efficient colour selector

--- js/main.js
+++ js/main.js
@@ -13,19 +13,46 @@ var imgArCounter = 0;
     function(ev){  	console.dir(ev); 
     }) 
 
-    document.querySelector(".red").addEventListener('click', function(ev){
-    document.querySelector('body').setAttribute('class', "redBack");    })
+    ////////////////////////////
 
-    document.querySelector(".green").addEventListener('click', function(ev){
-    document.querySelector('body').setAttribute('class', "greenBack");    })
+    // document.querySelector(".red").addEventListener('click', function(ev){
+    //     document.querySelector('body').setAttribute('class', "redBack");    
+    // });
 
-    document.querySelector(".blue").addEventListener('click', function(ev){
-    document.querySelector('body').setAttribute('class', "blueBack");    })
+    // document.querySelector(".green").addEventListener('click', function(ev){
+    //     document.querySelector('body').setAttribute('class', "greenBack");    
+    // });
+
+    // document.querySelector(".blue").addEventListener('click', function(ev){
+    //     document.querySelector('body').setAttribute('class', "blueBack");    
+    // });
     
-    document.querySelector(".reset").addEventListener('click', function(ev){
-    document.querySelector('body').setAttribute('class', "defaultBack");    })
+    // document.querySelector(".reset").addEventListener('click', function(ev){
+    //     document.querySelector('body').setAttribute('class', "defaultBack");    
+    // });
+
+
+    let colourButtons = document.querySelectorAll(".colPicker");
+
+    for(let i=0; i < colourButtons.length; i++){     
+        colourButtons[i].addEventListener("click", chgColour) 
+    }
+    const bodyElement = document.querySelector("body")
+    function chgColour(ev){     
+        console.dir(ev.target.classList)
+        let colourPicked = ev.target.classList[0] + "Back"
+        bodyElement.setAttribute("class", colourPicked)
+    }
+    
+
 
     //let myArray = ['cat', 'dog', 'mouse'];
+    
+    ////////////////////////////
+     
+
+
+
     let imageAr = ['images/view1.jpg', 'images/view2.jpg',
     'images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
     setInterval(chgImage, 4000); 
